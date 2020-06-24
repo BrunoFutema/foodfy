fa6306d32b5edcf200bc9f8db2c77b49c1ade387
Bruno Futema <bruno.futema@outlook.com>
2020-06-24 00:45:51 -0300

    refactor: refactoring styles.css and recipes pages

--- src/app/controllers/RecipeController.js
+++ src/app/controllers/RecipeController.js
@@ -1,14 +1,34 @@
+const Chef = require('../models/Chef');
 const Recipe = require('../models/Recipe');
 const LoadChefService = require('../services/LoadChefService');
 const LoadRecipeService = require('../services/LoadRecipeService');
 
 module.exports = {
-  index(req, res) {},
+  async index(req, res) {
+    try {
+      let recipes = await LoadRecipeService.load('recipes');
+  
+      const recipesPromise = recipes.map(async (recipe) => {
+        const chef = await Chef.find(recipe.chef_id);
+        
+        return {
+          ...recipe,
+          author: chef.name,
+        };
+      });
+  
+      recipes = await Promise.all(recipesPromise);
+  
+      return res.render('recipes/index', { recipes });
+    } catch (err) {
+      console.error(err);
+    }
+  },
   async create(req, res) {
     try {
       const chefs = await LoadChefService.load('chefs');
 
-      return res.render('recipes/create', { chefs });
+      return res.render('admin/recipes/create', { chefs });
     } catch (err) {
       console.error(err);
     }
@@ -35,11 +55,17 @@ module.exports = {
     try {
       const recipe_id = req.params.id;
 
+      console.log(req.params);
+
       let recipe = await LoadRecipeService.load('recipe', { where: { id: recipe_id } });
 
       const chef = await LoadChefService.load('chef', { where: { id: recipe.chef_id } });
 
       recipe.author = chef.name;
+
+      console.log(req.admin);
+
+      if (req.admin) return res.render('admin/recipes/show', { recipe });
 
       return res.render('recipes/show', { recipe });
     } catch (err) {
--- src/app/controllers/admin.js
+++ src/app/controllers/admin.js
@@ -2,20 +2,6 @@ module.exports = {
   index(req, res) {
     return res.render('admin/index');
   },
-  create(req, res) {
-    return res.render('admin/create');
-  },
-  post(req, res) {
-    const keys = Object.keys(req.body);
-  
-    for (key of keys) {
-      if (req.body[key] == '') return res.send('Please, fill all fields!');
-    }
-  
-    let { image, title, author, ingredients, preparations, information } = req.body;
-    
-    return res.redirect('/admin/recipes');
-  },
   show(req, res) {
     const { index } = req.params;
     const recipe = data.recipes[index];
@@ -26,16 +12,6 @@ module.exports = {
     recipe.information = recipe.information.split('\n').join('<br />');
   
     return res.render('admin/show', { recipe });
-  },
-  edit(req, res) {
-    const { index } = req.params;
-  
-    return res.render('admin/edit');
-  },
-  put(req, res) {
-    const { index } = req.body;
-  
-    return res.redirect(`/recipes/${index}`);
   },
   delete(req, res) {
     const { index } = req.body;
