bfc4f2f219d9526015e1c319792e40c6af7b161a
Bruno Futema <bruno.futema@outlook.com>
2020-06-24 17:23:36 -0300

    change: resolving bugs

--- src/app/models/Recipe.js
+++ src/app/models/Recipe.js
@@ -5,9 +5,38 @@ Base.init({ table: 'recipes' });
 
 module.exports = {
   ...Base,
+  async insert(fields) {
+    try {
+      let keys = [], values = [];
+
+      Object.keys(fields).map(key => {
+        keys.push(key);
+        if (typeof fields[key] === 'object') {
+          values.push(`'{${fields[key]}}'`);
+        } else {
+          values.push(`'${fields[key]}'`);
+        }
+      });
+
+      const query = `INSERT INTO ${this.table} (${keys.join(',')})
+        VALUES (${values.join(',')})
+        RETURNING id
+      `;
+
+      console.log(query);
+
+      const results = await db.query(query);
+
+      return results.rows[0].id;
+    } catch (err) {
+      console.error(err);
+    }
+  },
   async files(id) {
     const results = await db.query(`
-      SELECT * FROM files WHERE recipe_id = $1
+      SELECT files.* FROM files
+      LEFT JOIN recipe_files ON files.id = recipe_files.file_id
+      WHERE recipe_id = $1
     `, [id]);
 
     return results.rows;
