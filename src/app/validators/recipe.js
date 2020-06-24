1252fa804d9442fff17269cd2668baa0cff3abac
Bruno Futema <bruno.futema@outlook.com>
2020-06-24 17:24:55 -0300

    add: recipe validators of files

--- src/app/validators/recipe.js
+++ src/app/validators/recipe.js
@@ -1,6 +1,6 @@
 async function post(req, res, next) {
   const keys = Object.keys(req.body);
-  
+
   for (key of keys) {
     if (req.body[key] == '') return res.send('Por favor, volte e preencha todos os campos.');
   }
