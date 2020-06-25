6b7ea98a28c44d21e61e0a1ed2609e92542b999d
Bruno Futema <bruno.futema@outlook.com>
2020-06-25 19:17:47 -0300

    middlewares

--- src/app/middlewares/session.js
+++ src/app/middlewares/session.js
@@ -1,16 +1,23 @@
 function onlyUsers(req, res, next) {
-  // if (!req.session.userId) return res.redirect('/users/login');
+  if (!req.session.userId) return res.redirect('/session/login');
 
   next();
 };
 
-function isLoggedRedirectToUsers(req, res, next) {
-  if (req.session.userId) return res.redirect('/users');
+function onlyAdmin(req, res, next) {
+  if (!req.session.user.is_admin) return res.redirect('/');
+
+  next();
+};
+
+function isLoggedRedirectToProfile(req, res, next) {
+  if (req.session.userId) return res.redirect('/admin/profile');
 
   next();
 };
 
 module.exports = {
   onlyUsers,
-  isLoggedRedirectToUsers,
+  onlyAdmin,
+  isLoggedRedirectToProfile,
 };
